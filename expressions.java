6474af73131b931fca920f4b699cb7af31ac0806
cagdaskalak <64699562+cagdaskalak@users.noreply.github.com>
2021-10-12 12:10:56 +0300

    Add files via upload

---
+++ expressions.java
@@ -0,0 +1,7 @@
+public class expressions {
+    public static void main(String[] args) throws Exception {
+        int friends = 10;
+        friends--; // ++ / % de kullanabilirsin.
+        System.out.println(friends);
+    }
+}
